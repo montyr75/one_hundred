65950c03e7b7e42af6c006e1449fd4771811275d
Monty Rasmussen <montyr75@gmail.com>
2018-06-22 12:06:44 -0600

    cleanup

--- lib/src/models/global.dart
+++ lib/src/models/global.dart
@@ -2,7 +2,9 @@ import 'dart:html';
 
 import '../services/logger_service.dart';
 
-const String _appName = "dice";
-final bool _debugMode = window.location.host.contains('localhost');
+const String appName = "dice";
+final bool debugMode = window.location.host.contains('localhost');
 
-LoggerService newLogger() =>  LoggerService(appName: _appName, debugMode: _debugMode);
+LoggerService log;
+
+LoggerService newLogger() => log = LoggerService(appName: appName, debugMode: debugMode);
